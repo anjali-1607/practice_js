4470b4532c52221df832af48be1fa63d47d72862
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-05-27 12:22:38 +0530

    comment out to in

--- ArrayCrud.js
+++ ArrayCrud.js
@@ -24,7 +24,7 @@ while (true) {
     if (indexOfitems != -1) {
       items.splice(indexOfitems, 1);
     } else {
-      // console.log(indexOfitems)
+      console.log(indexOfitems);
       console.log("your item is not found: ");
       continue;
     }
